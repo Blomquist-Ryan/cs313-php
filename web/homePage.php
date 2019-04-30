74f64ef37cfe6100a5998bb78eaaf334e5aeb5ad
Ryan Blomquist <rblomquist96@gmail.com>
2019-04-30 11:06:44 -0600

    added more information about myself and hopefully fixed the external css file

--- web/homePage.php
+++ web/homePage.php
@@ -3,7 +3,7 @@
         <title>
             Ryan's Homepage
         </title>
-        <link rel="homepage" type="text/css" href="homepage.css">
+        <link rel="stylesheet" type="text/css" href="homepage.css">
     </head>
     <body>
         <h1>
@@ -12,6 +12,10 @@
         <img src="C:\Users\Ryan\Desktop/me.jpeg" alt="picture of me">
         <p>
             My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
-            I wrestled in High School and become pretty good almost going to state.
+            I wrestled in High School and become pretty good almost going to state. <br>
+            I love programming, I started in High School learning a little bit of java <br>
+            and decided to pursue a career in it. now I am proficient in c++, c# java <br>
+            html, and know php, javascript, and css. I have started a few projects with <br>
+            my roomate and we hope to finish one of them.
         </p>
     </body>
