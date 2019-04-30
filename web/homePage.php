54d5322213262bf99c9e4db9cf840bd3f9bf16e6
Ryan Blomquist <rblomquist96@gmail.com>
2019-04-30 10:46:45 -0600

    gave a title and brief description to make sure it works

---
+++ web/homePage.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Ryan's Homepage
+        </title>
+    </head>
+    <body>
+        <h1>
+            Homepage
+        </h1>
+        <p>
+            My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
+            I wrestled in High School and become pretty good almost going to state.
+        </p>
+    </body>
