5a18a5461af5b8a4da53869028cb95506d400b1b
Ryan Blomquist <rblomquist96@gmail.com>
2019-04-30 11:26:21 -0600

    added the assignments page for links to all assignments, links are not functioning. bulleted the links in the assignment page. created a link to the assignments page in the hompage

--- web/homePage.php
+++ web/homePage.php
@@ -14,8 +14,12 @@
             My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
             I wrestled in High School and become pretty good almost going to state. <br>
             I love programming, I started in High School learning a little bit of java <br>
-            and decided to pursue a career in it. now I am proficient in c++, c# java <br>
+            and decided to pursue a career in it. now I am proficient in c++, c#, java, <br>
             html, and know php, javascript, and css. I have started a few projects with <br>
             my roomate and we hope to finish one of them.
         </p>
+        <p>
+            assignments can be viewed <a href="assignments.html">here</a>.
+        </p>
+        
     </body>
