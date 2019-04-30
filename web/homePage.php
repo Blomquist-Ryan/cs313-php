8b131d7fcccf68dfffe993b8c39ac7c925fb39d4
Ryan Blomquist <rblomquist96@gmail.com>
2019-04-30 10:57:48 -0600

    added an image and link to css file

--- web/homePage.php
+++ web/homePage.php
@@ -3,11 +3,13 @@
         <title>
             Ryan's Homepage
         </title>
+        <link rel="homepage" type="text/css" href="homepage.css">
     </head>
     <body>
         <h1>
             Homepage
         </h1>
+        <img src="C:\Users\Ryan\Desktop/me.jpeg" alt="picture of me">
         <p>
             My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
             I wrestled in High School and become pretty good almost going to state.
