88cc6907f4f90ecd5af3537c38be4faa7b51fec2
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-01 13:04:43 -0600

    possible fix so the image displays

--- web/homePage.php
+++ web/homePage.php
@@ -9,7 +9,7 @@
         <h1>
             Homepage
         </h1>
-        <img src="me.jpeg" alt="picture of me">
+        <img src="../cs313-php/web/me.jpeg" alt="picture of me">
         <p>
             My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
             I wrestled in High School and become pretty good almost going to state. <br>
