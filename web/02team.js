b1b4e30b22230e72eae3be9ef3e1d6f88387473c
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-01 12:18:45 -0600

    wrote the java script to fire an alert for the click me button and change the color of the first div

---
+++ web/02team.js
@@ -0,0 +1,8 @@
+function fireAlert(){
+    alert("Clicked!");
+}
+
+function colorChange(){
+    var color = document.getElementById("colorChange").value;
+    document.getElementById("first").style.backgroundColor = color;
+}
