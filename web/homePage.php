4f016e7299e0d7a2f68188b66055cd2ce3b6c5e4
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-01 13:00:52 -0600

    changed the img src

--- web/homePage.php
+++ web/homePage.php
@@ -9,7 +9,7 @@
         <h1>
             Homepage
         </h1>
-        <img src="C:\Users\Ryan\Desktop/me.jpeg" alt="picture of me">
+        <img src="me.jpeg" alt="picture of me">
         <p>
             My name is Ryan Blomquist. I am from Houston TX, but I spent several years in California. <br>
             I wrestled in High School and become pretty good almost going to state. <br>
