1f9805110c73e90318ac68cd3d1dd7a216047a29
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-01 14:12:42 -0600

    fixed the doctype of the images

--- web/homepage.js
+++ web/homepage.js
@@ -7,15 +7,15 @@ function changePicture(){
             pic++;
             break;
          case 2:
-            document.getElementById("me").src="modeling1.JPG";
+            document.getElementById("me").src="modeling1.jpg";
             pic++;
             break;
         case 3:
-            document.getElementById("me").src="modeling2.JPG";
+            document.getElementById("me").src="modeling2.jpg";
             pic++;
             break;
         case 4:
-            document.getElementById("me").src="modeling3.JPG";
+            document.getElementById("me").src="modeling3.jpg";
             pic = 1;
             break;
 
