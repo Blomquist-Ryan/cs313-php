7ef3c7b4f0e038a408cf372ff6ea19754d98578a
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-07 14:15:03 -0600

    attempted to see if the html will work

--- web/browse.php
+++ web/browse.php
@@ -16,6 +16,7 @@ session_start();
     <body>
     <!-- <a href="cart.php">cart</a> -->
 
+    <p>there should be some text here</p>
     <input type="text" name="" id="thing">
     <input type="button" value="Add Item" onclick="add()">
     <?php echo $_SESSION["item"];
