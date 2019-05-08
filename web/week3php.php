a4d1c274bfef8bb14477c643c2413767317b11e7
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-08 16:40:54 -0600

    added more of the php elements

--- web/week3php.php
+++ web/week3php.php
@@ -1,3 +1,3 @@
-<?php
-echo "username:" . $_POST["Name"] . "<br>";
-?>
+Username:  <?php echo  $_POST["Name"] ;?> <br>
+MailTo: <?php echo $_POST["email"] ; ?> <br>
+Major: <?php echo $_POST["radio"]; ?> <br>
