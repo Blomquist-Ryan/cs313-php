f781b5f619bc585784014dcae9c97652e7e80fe7
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-08 17:09:50 -0600

    added the htmls special chars to fix the array display

--- web/week3php.php
+++ web/week3php.php
@@ -2,4 +2,9 @@ Username:  <?php echo  $_POST["name"] ;?> <br>
 MailTo: <?php echo $_POST["email"] ; ?> <br>
 Major: <?php echo $_POST["major"]; ?> <br>
 Comments: <?php echo $_POST["comments"]; ?> <br>
-visited: <?php foreach($_POST["locations"] as $place){ echo $place ."</br>";} ?>
+
+visited: <?php if(!empty($_POST["locations"])){
+     foreach($_POST["locations"] as $place){
+          echo htmlspecialchars($place) ."</br>";
+            }
+        } ?>
