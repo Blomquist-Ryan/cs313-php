a01c84303cc6440ee8d774874f7400bfcd832d3f
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-08 16:49:48 -0600

    finished adding locations and comments

--- web/week3php.php
+++ web/week3php.php
@@ -1,3 +1,5 @@
-Username:  <?php echo  $_POST["Name"] ;?> <br>
+Username:  <?php echo  $_POST["name"] ;?> <br>
 MailTo: <?php echo $_POST["email"] ; ?> <br>
 Major: <?php echo $_POST["major"]; ?> <br>
+Comments: <?php echo $_POST["comments"]; ?> <br>
+visited: <?php echo $_POST["locations"]; ?>
