9fb944f66671705edf80e7882b3e2e4d9a0877af
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-08 16:56:23 -0600

    fixed the array to display the array

--- web/week3php.php
+++ web/week3php.php
@@ -2,4 +2,4 @@ Username:  <?php echo  $_POST["name"] ;?> <br>
 MailTo: <?php echo $_POST["email"] ; ?> <br>
 Major: <?php echo $_POST["major"]; ?> <br>
 Comments: <?php echo $_POST["comments"]; ?> <br>
-visited: <?php echo $_POST["locations"]; ?>
+visited: <?php foreach($_POST["locations"] as $place){ echo $place ."</br>";} ?>
