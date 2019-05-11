65ad71248d54e010cb8fe85c8ebacefbb048c67d
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-11 16:22:59 -0600

    added htmlspecialchar to prevent malicious injections

--- web/confirm.php
+++ web/confirm.php
@@ -1,8 +1,8 @@
 <?php session_start() ;
- $_SESSION["address"] = $_POST["address"];
- $_SESSION["city"] = $_POST["city"];
- $_SESSION["state"] = $_POST["state"];
- $_SESSION["zip"] = $_POST["zip"];
+ $_SESSION["address"] = htmlspecialchar($_POST["address"]);
+ $_SESSION["city"] = htmlspecialchar($_POST["city"]);
+ $_SESSION["state"] = htmlspecialchar($_POST["state"]);
+ $_SESSION["zip"] = htmlspecialchar($_POST["zip"]);
 
 echo 'Your shopping cart contains: <br> <form  method="POST" action="cart.php" >';
 foreach($_SESSION["cart"] as $product){
