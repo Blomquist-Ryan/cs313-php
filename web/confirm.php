7b8f7dead3763c2a21aece00fa6dcdddf93cc0ee
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-11 16:24:22 -0600

    fixed specialchar

--- web/confirm.php
+++ web/confirm.php
@@ -1,15 +1,15 @@
 <?php session_start() ;
- $_SESSION["address"] = htmlspecialchar($_POST["address"]);
- $_SESSION["city"] = htmlspecialchar($_POST["city"]);
- $_SESSION["state"] = htmlspecialchar($_POST["state"]);
- $_SESSION["zip"] = htmlspecialchar($_POST["zip"]);
+ $_SESSION["address"] = $_POST["address"];
+ $_SESSION["city"] = $_POST["city"];
+ $_SESSION["state"] = $_POST["state"];
+ $_SESSION["zip"] = $_POST["zip"];
 
 echo 'Your shopping cart contains: <br> <form  method="POST" action="cart.php" >';
 foreach($_SESSION["cart"] as $product){
     echo $product;
     echo "<br>";
 }
-echo "the shipping addres is:" . $_SESSION["address"] . " " . $_SESSION["city"] . ", " .  $_SESSION["state"] . ", " .  $_SESSION["zip"]
+echo "the shipping addres is:" . htmlspecialchar($_SESSION["address"]) . " " . $_SESSION["city"] . ", " .  $_SESSION["state"] . ", " .  $_SESSION["zip"]
 ?>
 <html>
     <head>
