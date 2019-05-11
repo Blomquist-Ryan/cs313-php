f1f297d1dbb13eaa2c80abedc168292f2595b887
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-11 12:01:20 -0600

    added links to browse and checkout and the assignment

--- web/cart.php
+++ web/cart.php
@@ -1,9 +1,17 @@
 <?php session_start() ;
 
-echo "Your shopping cart contains:";
+echo "Your shopping cart contains: <br>";
 foreach($_SESSION["cart"] as $item){
     echo $item;
     echo "<br>";
 }
 
 ?>
+<html>
+    <head>
+        <title>shopping cart</title>
+    </head>
+    <body>
+        <a href="checkout.php">checkout</a>
+        <a href="browse.php">continue Shopping</a>
+    </body>
