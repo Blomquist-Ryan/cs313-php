056276e4216d5aea4642768a7deea4fa962d5831
Ryan Blomquist <rblomquist96@gmail.com>
2019-05-24 15:27:39 -0600

    copied the instructor code to see if that fixes an error of missing driver

--- web/access.php
+++ web/access.php
@@ -1,22 +1,24 @@
 <?php
-try{
-    $dbUrl = getenv('DATABASE_URL');
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPass = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"]);
-
-    $db = new PDO("psql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $ex)
+try
 {
-    echo 'error!: ' . $ex->getMessage();
-    die();
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
 foreach($db->query('SELECT * FROM person') as $row)
 {
