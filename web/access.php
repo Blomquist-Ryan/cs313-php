b0ed4be7d9c06065785b3a9db2428c48917fc02e
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-01 10:33:49 -0600

    queried the rest of the tables

--- web/access.php
+++ web/access.php
@@ -11,7 +11,8 @@ $id = htmlspecialchars($_GET['id']);
 try{
 
 
-$stmt  = $db->prepare('SELECT * FROM seg_fat s, muscle m  WHERE s.id=:id AND m.id=:id');
+$stmt  = $db->prepare('SELECT * FROM seg_fat s, muscle m, comp_analysis c, history h, lean l, obesity o, person p, 
+  WHERE s.id=:id AND m.id=:id AND c.id=:id AND h.id=:id AND l.id=:id AND o.id=:id AND p.user_id=:id');
 $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
