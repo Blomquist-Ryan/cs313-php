301dbee80518752256cb5b12afe44e52afc467b5
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-01 14:02:09 -0600

    Wrote all the php to add to the database

--- database/database.sql
+++ database/database.sql
@@ -4,11 +4,11 @@ CREATE TABLE Person
 	Name VARCHAR(50) NOT NULL,
 	INumber INT NOT NULL,
 	date DATE NOT NULL,
-	Comp_analysis_id INT NOT NULL REFERENCES Comp_analysis(id),
-	Muscle_fat_id INT NOT NULL REFERENCES Muscle(id),
-	Obesity_id INT NOT NULL REFERENCES Obesity(id),
-	Lean_id INT NOT NULL REFERENCES Lean(id),
-	History_id INT NOT NULL REFERENCES History(id)
+	Comp_analysis_id SERIAL NOT NULL REFERENCES Comp_analysis(id),
+	Muscle_fat_id SERIAL NOT NULL REFERENCES Muscle(id),
+	Obesity_id SERIAL NOT NULL REFERENCES Obesity(id),
+	Lean_id SERIAL NOT NULL REFERENCES Lean(id),
+	History_id SERIAL NOT NULL REFERENCES History(id)
 );
 CREATE TABLE Comp_analysis
 (
@@ -30,7 +30,7 @@ CREATE TABLE Obesity
 	Id SERIAL NOT NULL PRIMARY KEY,
 	Bmi REAL NOT NULL,
 	PBF REAL NOT NULL,
-	Seg_fat_id INT NOT NULL REFERENCES Seg_fat(id)
+	Seg_fat_id SERIAL NOT NULL REFERENCES Seg_fat(id)
 );
 CREATE TABLE Seg_fat
 (
--- web/DBInsert.php
+++ web/DBInsert.php
@@ -2,6 +2,9 @@
 require("Dbconnect.php");
 $db = get_db();
 
+$name = htmlspecialchars($_POST['name']);
+$inum = htmlspecialchars($_POST['iNum']);
+
 $date = htmlspecialchars($_POST['date']);
 $intra = htmlspecialchars($_POST['intra']);
 $extra = htmlspecialchars($_POST['extra']);
@@ -29,7 +32,54 @@ $llegl = htmlspecialchars($_POST['LlegL']);
 
 $water = htmlspecialchars($_POST['water']);
 
-var_dump($_POST);
 
-// $stmt = $db->prepare('INSERT INTO ')
+$stmt = $db->prepare('INSERT INTO person(name, inumber, date) Values(:name, :inum, :date);');
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':inum', $inum, PDO::PARAM_INT);
+$stmt->bindValue(':date', $date, PDO::PARAM_STR);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO comp_analysis(intra, extra, dry_lean, Body_fat) Values(:intra, :extra, :drylean, :bodyfat);');
+$stmt->bindValue(':intra', $intra, PDO::PARAM_INT);
+$stmt->bindValue(':extra', $extra, PDO::PARAM_INT);
+$stmt->bindValue(':drylean', $drylean, PDO::PARAM_INT);
+$stmt->bindValue(':bodyfat', $fatmass, PDO::PARAM_INT);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO muscle(weight, smm, fat) Values(:weight, :smm, :fat);');
+$stmt->bindValue(':weight', $weight, PDO::PARAM_INT);
+$stmt->bindValue(':smm', $smm, PDO::PARAM_INT);
+$stmt->bindValue(':fat', $fatmass, PDO::PARAM_INT);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO obesity(bmi, pbf) Values(:bmi, :pbf);');
+$stmt->bindValue(':bmi', $name, PDO::PARAM_INT);
+$stmt->bindValue(':pbf', $name, PDO::PARAM_INT);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO seg_fat(rarm, larm, trunk, rleg, lleg) Values(:rarm, :larm, :trunk, :rleg, :lleg);');
+$stmt->bindValue(':rarm', $rarm, PDO::PARAM_INT);
+$stmt->bindValue(':larm', $larm, PDO::PARAM_INT);
+$stmt->bindValue(':trunk', $trunk, PDO::PARAM_INT);
+$stmt->bindValue(':rleg', $rleg, PDO::PARAM_INT);
+$stmt->bindValue(':lleg', $lleg, PDO::PARAM_INT);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO lean(rarml, larml, trunkl, rlegl, llegl) Values(:rarml, :larml, :trunkl, :rlegl, :llegl);');
+$stmt->bindValue(':rarml', $rarml, PDO::PARAM_INT);
+$stmt->bindValue(':larml', $larml, PDO::PARAM_INT);
+$stmt->bindValue(':trunkl', $trunkl, PDO::PARAM_INT);
+$stmt->bindValue(':rlegl', $rlegl, PDO::PARAM_INT);
+$stmt->bindValue(':llegl', $llegl, PDO::PARAM_INT);
+$stmt->execute();
+
+$stmt = $db->prepare('INSERT INTO muscle(weight, smm, pbf, water) Values(:weight :smm, :pbf, :water);');
+$stmt->bindValue(':weight', $weight, PDO::PARAM_INT);
+$stmt->bindValue(':smm', $smm, PDO::PARAM_INT);
+$stmt->bindValue(':pbf', $name, PDO::PARAM_INT);
+$stmt->bindValue(':water', $water, PDO::PARAM_INT);
+$stmt->execute();
+
+header("location: inbody.php");
+die();
 ?>
