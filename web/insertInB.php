63cc0d348f0b05e72476b1f429291ca75595138c
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-01 00:17:25 -0600

    wrote all the insert html

---
+++ web/insertInB.php
@@ -0,0 +1,46 @@
+<?php 
+require("Dbconnect.php");
+$db = get_db();
+?>
+<!DOCTYPE html>
+<html>
+  <body>
+    <form action="access.php" method="post">
+        <h3>Body Composition Analysis</h3>
+  Intracellular Water: <input type="text" name="intra" id="inta" required> <br>
+  Extracellular Water: <input type="text" name="extra" id="extra" required> <br>
+  Dry Lean Mass: <input type="text" name="dry_lean" id="dry_lean" required> <br>
+  Body Fat Mass: <input type="text" name="fatMass" id="fatMass" required> <br>
+  <hr>
+  <h3>Muscle-Fat Analysis</h3>
+  Weight: <input type="text" name="weight" id="weight" required> <br>
+  Skeletal Muscle Mass: <input type="text" name="SMM" id="SMM" required> <br>
+  Body Fat Mass: <input type="text" name="fat" id="fat" required> <br>
+  <hr>
+  <h3>Obesity Analysis</h3>
+  BMI: <input type="text" name="BMI" id="BMI" required> <br>
+  Percent Body Fat: <input type="text" name="pbf" id="pbf" required> <br>
+  <hr>
+  <h3>Segmental Fat Analysis</h3>
+  Right Arm: <input type="text" name="RarmF" id="RarmF" required> <br>
+  Left Arm: <input type="text" name="LarmF" id="LarmF" required> <br>
+  Trunk: <input type="text" name="trunkF" id="trunkF" required> <br>
+  Right Leg: <input type="text" name="RlegF" id="RlegF" required> <br>
+  Left Leg: <input type="text" name="LlegF" id="LlegF" required> <br>
+  <hr>
+  <h3>Segmental Lean Analysis</h3>
+  Right Arm: <input type="text" name="RarmL" id="RarmL" required> <br>
+  Left Arm: <input type="text" name="LarmL" id="LarmL" required> <br>
+  Trunk: <input type="text" name="trunkL" id="trunkL" required> <br>
+  Right Leg: <input type="text" name="RlegL" id="RlegL" required> <br>
+  Left Leg: <input type="text" name="LlegL" id="LlegL" required> <br>
+  <hr>
+  <h3>ECW/TBW Analysis</h3>
+  ECW/TBW: <input type="text" name="water" id="water" required> <br>
+
+
+
+  <input type="submit" name="submit" value="submit">
+</form>
+  </body>
+</html>
