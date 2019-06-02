2395bf83f367beae771f78d2225a36ae83406604
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-01 20:04:25 -0600

    another attempt

--- web/DBInsert.php
+++ web/DBInsert.php
@@ -32,7 +32,7 @@ $llegl = htmlspecialchars($_POST['LlegL']);
 
 
 $water = htmlspecialchars($_POST['water']);
-// $id +=1;
+
 
 echo "$name <br>
 $inum <br>
@@ -70,13 +70,14 @@ try{
 
 
 
-/* $stmtc = $db->prepare('INSERT INTO comp_analysis(intra, extra, dry_lean, Body_fat) Values(:intra, :extra, :drylean, :bodyfat);');
+ $stmtc = $db->prepare('INSERT INTO comp_analysis(intra, extra, dry_lean, Body_fat) Values(:intra, :extra, :drylean, :bodyfat);');
 $stmtc->bindValue(':intra', $intra, PDO::PARAM_INT);
 $stmtc->bindValue(':extra', $extra, PDO::PARAM_INT);
 $stmtc->bindValue(':drylean', $drylean, PDO::PARAM_INT);
 $stmtc->bindValue(':bodyfat', $fat, PDO::PARAM_INT);
 $stmtc->execute();
 
+/*
 $stmtm = $db->prepare('INSERT INTO muscle(weight, smm, fat) Values(:weight, :smm, :fat);');
 $stmtm->bindValue(':weight', $weight, PDO::PARAM_INT);
 $stmtm->bindValue(':smm', $smm, PDO::PARAM_INT);
@@ -110,15 +111,15 @@ $stmtm->bindValue(':smm', $smm, PDO::PARAM_INT);
 $stmtm->bindValue(':pbf', $name, PDO::PARAM_INT);
 $stmtm->bindValue(':water', $water, PDO::PARAM_INT);
 $stmtm->execute(); 
-*/
+
 $stmtp = $db->prepare('INSERT INTO person(name, inumber) Values(:name, :inum);');
-$stmtp->bindValue(':name', $name/* , PDO::PARAM_STR */);
-$stmtp->bindValue(':inum', $inum/* , PDO::PARAM_INT */);
+$stmtp->bindValue(':name', $name , PDO::PARAM_STR );
+$stmtp->bindValue(':inum', $inum , PDO::PARAM_INT );
  $stmtp->bindValue(':date', $date, PDO::PARAM_STR);
 $stmtp->execute(); 
 
 header("location: inbody.php");
-die();
+die();*/
  }
 catch(PDOException $ex)
 {
