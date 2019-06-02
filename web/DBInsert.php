c8d17bfb7e01f6d2832b502eebe188a1253d7009
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-01 21:29:27 -0600

    fixed the issues hopefully

--- web/DBInsert.php
+++ web/DBInsert.php
@@ -145,7 +145,7 @@ catch(PDOException $ex)
 
 try
 {
-$stmtp = $db->prepare('INSERT INTO person(username, inumber, datte) Values(:username :inum, :datte);');
+$stmtp = $db->prepare('INSERT INTO person(username, inumber, datte) Values(:username, :inum, :datte);');
 $stmtp->bindValue(':username', $username, PDO::PARAM_STR );
 $stmtp->bindValue(':inum', $inum, PDO::PARAM_INT );
  $stmtp->bindValue(':datte', $datte, PDO::PARAM_STR);
