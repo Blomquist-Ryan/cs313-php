a1921e6bc6878367eab588a555c7857fcbb8985b
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-05 16:46:08 -0600

    removed date to see if it works

--- web/DBInsert.php
+++ web/DBInsert.php
@@ -104,10 +104,10 @@ $stmtl->execute();
 
 
 
-$stmtp = $db->prepare('INSERT INTO person(username, inumber/* , datte) */) Values(:username, :inum/* , $datte */);');
+$stmtp = $db->prepare('INSERT INTO person(username, inumber , datte) Values(:username, :inum, $datte);');
 $stmtp->bindValue(':username', $username, PDO::PARAM_STR );
 $stmtp->bindValue(':inum', $inum, PDO::PARAM_INT );
-/*  $stmtp->bindValue(':datte', $datte, PDO::PARAM_STR); */
+ $stmtp->bindValue(':datte', $datte, PDO::PARAM_STR);
 $stmtp->execute(); 
 
 header("location: inbody.php");
