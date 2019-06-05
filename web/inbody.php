b06e41acde62984b9edae39027b156edf74a0286
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-05 16:33:29 -0600

    removed date to see if it works

--- web/inbody.php
+++ web/inbody.php
@@ -2,14 +2,14 @@
 require("Dbconnect.php");
 $db = get_db();
 
-$query = 'SELECT id, username, datte FROM person';
+$query = 'SELECT id, username/* , datte  */FROM person';
 
 $stmt = $db->prepare($query);
 $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 $name = $user['username'];
 $num = $user['id'];
-$datte = $user['datte'];
+// $datte = $user['datte'];
 
 
 $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -29,7 +29,7 @@ $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
       foreach($tests as $test)
       {
           $num = $test['id'];
-          $datte = $test['datte'];
+        //   $datte = $test['datte'];
           echo " <a href='access.php?id=$num'> Test $num on $datte <br>";
       }
       ?>
